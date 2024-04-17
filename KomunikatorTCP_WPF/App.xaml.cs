50537b2fddd77b5bd536441f755935c931db5ce4
kuadalma <adammarzec2808@gmail.com>
2024-04-17 17:43:39 +0200

    Add project files.

---
+++ KomunikatorTCP_WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace KomunikatorTCP_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
